42923cfa0870d3b4f13ff9c4ba13cbca815482d6
reich452 <reich452@regis.edu>
2017-10-10 10:05:36 -0600

    Initial Commit

---
+++ PokemonHybridiOS15/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokemonHybridiOS15
+//
+//  Created by Nick Reichard on 10/10/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokemonHybridiOS15/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokemonHybridiOS15
+//
+//  Created by Nick Reichard on 10/10/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
